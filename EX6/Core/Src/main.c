591f2be59edb88aabffea8b78999d5a0e42d71e6
toniminhkhoi <khoi.pham2005ktmt@hcmut.edu.vn>
2025-09-15 09:21:26 +0700

    LAB1.1

--- EX6/Core/Src/main.c
+++ EX6/Core/Src/main.c
@@ -4,11 +4,26 @@
   * @file           : main.c
   * @brief          : Main program body
   ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
+const uint16_t ledPins[12] = {
+    GPIO_PIN_4, GPIO_PIN_5, GPIO_PIN_6, GPIO_PIN_7,
+    GPIO_PIN_8, GPIO_PIN_9, GPIO_PIN_10, GPIO_PIN_11,
+    GPIO_PIN_12, GPIO_PIN_13, GPIO_PIN_14, GPIO_PIN_15
+};
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
@@ -31,20 +46,14 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-const uint16_t ledPins[12] = {
-    GPIO_PIN_4, GPIO_PIN_5, GPIO_PIN_6, GPIO_PIN_7,
-    GPIO_PIN_8, GPIO_PIN_9, GPIO_PIN_10, GPIO_PIN_11,
-    GPIO_PIN_12, GPIO_PIN_13, GPIO_PIN_14, GPIO_PIN_15
-};
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
-void setNumberOnClock(int num);
-void clearAllClock(void);
-void clearNumberOnClock(int num);
+
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -59,16 +68,29 @@ void clearNumberOnClock(int num);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-  int hour = 2, minute = 59, second = 40;
+	int num=0;
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
-  HAL_Init();
-  SystemClock_Config();
-  MX_GPIO_Init();
 
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
-  clearAllClock();
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -77,35 +99,13 @@ int main(void)
   {
     /* USER CODE END WHILE */
 
+	  HAL_GPIO_WritePin(GPIOA, ledPins[num], GPIO_PIN_RESET);
+	  HAL_Delay(1000);
+	  HAL_GPIO_WritePin(GPIOA, ledPins[num++], GPIO_PIN_SET);
+	  if(num>=12){num=0;}
+
+
     /* USER CODE BEGIN 3 */
-    clearAllClock();
-
-    int second_led_index = second / 5;
-    int minute_led_index = minute / 5;
-
-    // CÔNG THỨC TÍNH GIỜ
-    int hour_led_index = hour % 12;
-
-    setNumberOnClock(second_led_index);
-    setNumberOnClock(minute_led_index);
-    setNumberOnClock(hour_led_index);
-
-    // Cập nhật thời gian
-    second++;
-    if (second >= 60) {
-        second = 0;
-        minute++;
-        if (minute >= 60) {
-            minute = 0;
-            hour++;
-            if (hour > 12) {
-                hour = 1;
-            }
-        }
-    }
-
-    // Chờ 1 giây
-    HAL_Delay(1000);
   }
   /* USER CODE END 3 */
 }
@@ -132,7 +132,6 @@ void SystemClock_Config(void)
   }
   /** Initializes the CPU, AHB and APB buses clocks
   */
-  // SỬA LẠI CÁC DÒNG DƯỚI ĐÂY
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
@@ -145,6 +144,7 @@ void SystemClock_Config(void)
     Error_Handler();
   }
 }
+
 /**
   * @brief GPIO Initialization Function
   * @param None
@@ -172,30 +172,10 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 4 */
-void setNumberOnClock(int num)
-{
-  if (num >= 0 && num < 12)
-  {
-    HAL_GPIO_WritePin(GPIOA, ledPins[num], GPIO_PIN_RESET);
-  }
-}
-
-void clearAllClock(void)
-{
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7 |
-		  GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11 |
-		  GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15, GPIO_PIN_SET);
-}
-void clearNumberOnClock(int num)
-{
-  if (num >= 0 && num < 12)
-  {
-    HAL_GPIO_WritePin(GPIOA, ledPins[num], GPIO_PIN_SET);
-  }
-}
 
 /* USER CODE END 4 */
 
@@ -217,7 +197,7 @@ void Error_Handler(void)
 #ifdef  USE_FULL_ASSERT
 /**
   * @brief  Reports the name of the source file and the source line number
-  * where the assert_param error has occurred.
+  *         where the assert_param error has occurred.
   * @param  file: pointer to the source file name
   * @param  line: assert_param error line source number
   * @retval None
@@ -230,3 +210,5 @@ void assert_failed(uint8_t *file, uint32_t line)
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
